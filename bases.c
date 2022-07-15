ccc65c804c269ba8926a108940916bfb1be82bc1
TabbieManywanda <achiengtabitha@gmail.com>
2022-07-15 07:24:06 -0700

    Prints numbers in various bases

---
+++ bases.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/**
+ *printHexa - prints a number in hexadecimal lowercase
+ *@l: arguments
+ *@f: pointer to struct
+ *
+ *Return: number of chars printed
+ */
+int printHexa(va_list l, indicators_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 1);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+	return (count);
+}
+
+/**
+ *hexaBig - prints number in hexadecimal uppercase
+ *@l:arguments
+ *@f: pointer to structs
+ *
+ *Return: the number of chars printed
+ *
+ */
+int hexaBig(va_list l, indicators_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0X");
+	count += _puts(str);
+	return (count);
+}
+
+/**
+ *terrible twos - prints a number in base 2
+ *@l: arguments
+ *@f: pointer to struct
+ *
+ *Return: number of char printed
+ */
+int terribletwos(va_list l, indicators_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 2, 0);
+
+	(void)f;
+	return (_puts(str));
+}
+
+/**
+ *eightPrint - print number in base 8
+ *@l: arguments
+ *@f: pointer to struct
+ *
+ *Return: number of char printed
+ */
+int eightPrint(va_list l, indicators_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 8, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _putchar('0');
+	count += _puts(str);
+	return (count);
+}
